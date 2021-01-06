47ba28b8318329d5a4f55cdf66ac9bc795a4d517
moisesmengo <moisesmengo@live.com>
2021-01-06 13:17:25 -0300

    estilizando menu

--- src/views/usuario/Usuario.vue
+++ src/views/usuario/Usuario.vue
@@ -30,5 +30,38 @@ export default {
 </script>
 
 <style scoped>
-  
+  .usuario{
+    display: grid;
+    grid-template-columns: minmax(140px, 200px) 1fr;
+    max-width: 900px;
+    margin: 40px auto;
+    grid-gap: 30px;
+    padding: 20px;
+  }
+  .sidenav a, .sidenav button{
+    padding: 10px;
+    display: block;
+    background-color: #f4f7fc;
+    margin-bottom: 10px;
+    border-radius: 4px;
+  }
+  .sidenav a.router-link-exact-active, .sidenav a:hover, .sidenav button:hover{
+    background-color: #87f;
+    color: #fff;
+  }
+  .sidenav button{
+    border: none;
+    width: 100%;
+    font-size: 1rem;
+    text-align: left;
+    font-family: 'Comfortaa', cursive;
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 500px){
+    .usuario{
+      grid-template-columns: 1fr;
+      margin: 0px auto;
+    }
+  }
 </style>
