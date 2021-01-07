329f88d61dff8e6bbd2b5587fb31bdbd069789d0
moisesmengo <moisesmengo@live.com>
2021-01-07 20:09:28 -0300

    listando produtos do usuario

---
+++ src/views/usuario/ProdutoAdicionar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <p>produto adcionar</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'ProdutoAdicionar'
+}
+</script>
+
+<style scoped>
+    
+</style>
--- src/views/usuario/UsuarioProduto.vue
+++ src/views/usuario/UsuarioProduto.vue
@@ -1,9 +1,35 @@
 <template>
-  <p>Lista de produtod</p>
+  <section>
+    <h2>Adicionar Produto</h2>
+    <ProdutoAdicionar />
+    <h2>Seus Produtos</h2>
+    {{usuario_produtos}}
+  </section>
 </template>
 <script>
+import ProdutoAdicionar from './ProdutoAdicionar'
+
+import {mapState, mapActions} from 'vuex'
+
 export default {
-  name: "UsuarioProdutos"
+  name: "UsuarioProdutos",
+  components:{ProdutoAdicionar},
+  computed:{
+    ...mapState(['login', 'usuario', 'usuario_produtos'])
+  },
+  methods:{
+    ...mapActions(["getUsuarioProdutos"])
+  },
+  watch:{
+    login(){
+      this.getUsuarioProdutos()
+    }
+  },
+  created(){
+    if(this.login){
+      this.getUsuarioProdutos()
+    }
+  }
 }
 </script>
 <style scoped>
